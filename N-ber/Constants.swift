324bac84493652637415c504a7d02f9c182b0489
SeymaArslan <seymarslannn@gmail.com>
2023-08-17 17:17:39 +0300

    Create User.swift with Firebase

---
+++ N-ber/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  N-ber
+//
+//  Created by Seyma on 17.08.2023.
+//
+
+import Foundation
+
+public let kCURRENTUSER = "currentUser"
---
+++ N-ber/Model/User.swift
@@ -0,0 +1,43 @@
+//
+//  User.swift
+//  N-ber
+//
+//  Created by Seyma on 17.08.2023.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+struct User: Codable, Equatable { // Equatable is to control two equal users ne zaman 2 kullanıcı sağlayacağımızı kontrol edeceğiz
+    var id = ""
+    var username: String
+    var email: String
+    var pushId = ""
+    var avatarLink = ""
+    var status: String
+    
+    static var currentId: String {
+        return Auth.auth().currentUser!.uid
+    }
+    
+    static var currentUser: User? {
+        if Auth.auth().currentUser != nil {
+            if let dictionary = UserDefaults.standard.data(forKey: kCURRENTUSER) {
+                let decoder = JSONDecoder()
+                do{
+                    let userObject = try decoder.decode(User.self, from: dictionary)
+                    return userObject
+                } catch {
+                    print("Error decoding user from user defaults ", error.localizedDescription)
+                }
+            }
+        }
+        return nil
+    }
+    
+    static func == (lhs: User, rhs: User) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+}
