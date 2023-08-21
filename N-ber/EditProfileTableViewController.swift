fe8418cfdbc2ae139bd13afbeb44fc6b0b8053e0
SeymaArslan <seymarslannn@gmail.com>
2023-08-21 14:56:59 +0300

    Photo editing for avatarImage is complete

--- N-ber/EditProfileTableViewController.swift
+++ N-ber/EditProfileTableViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Gallery
+import ProgressHUD
 
 class EditProfileTableViewController: UITableViewController {
 
@@ -13,6 +15,8 @@ class EditProfileTableViewController: UITableViewController {
     @IBOutlet weak var avatarImageView: UIImageView!
     @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var userNameTextField: UITextField!
+    
+    var gallery: GalleryController!
     
     //MARK: - ViewLifeCycle
     override func viewDidLoad() {
@@ -42,6 +46,13 @@ class EditProfileTableViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true) // satır seçildiğinde seçili olarak kalıyor bunun böyle olmaması için bu kodu yazdık
     }
     
+    //MARK: - IBActions
+    
+    @IBAction func editButtonPressed(_ sender: Any) {
+        // show gallery
+        showImageGallery()
+    }
+    
     //MARK: - UpdateUI
     private func showUserInfo() {
         if let user = User.currentUser {
@@ -58,6 +69,18 @@ class EditProfileTableViewController: UITableViewController {
     private func configureTextField() {
         userNameTextField.delegate = self
         userNameTextField.clearButtonMode = .whileEditing
+    }
+    
+    //MARK: - Gallery
+    private func showImageGallery() {
+        self.gallery = GalleryController()
+        self.gallery.delegate = self
+        
+        Config.tabsToShow = [.imageTab, .cameraTab]
+        Config.Camera.imageLimit = 1
+        Config.initialTab = .imageTab
+        
+        self.present(gallery, animated: true, completion: nil)
     }
 }
 
@@ -76,4 +99,35 @@ extension EditProfileTableViewController: UITextFieldDelegate {
         }
         return true
     }
+}
+
+extension EditProfileTableViewController: GalleryControllerDelegate {
+    func galleryController(_ controller: Gallery.GalleryController, didSelectImages images: [Gallery.Image]) {
+        // kullanıcı birden çok görüntü dizisi seçtiğinde bizi bilgilendirir. info.plist ten camera izni almayı unutma (Privacy - Photo Library Usage Description, Privacy - Media Library Usage Description, Privacy - Camera Usage Description)
+        if images.count > 0 {
+            images.first!.resolve { (avatarImage) in
+                if avatarImage != nil {
+                    self.avatarImageView.image = avatarImage
+                } else {
+                    ProgressHUD.showError("Fotoğraf seçmediniz!")
+                }
+            }
+        }
+        controller.dismiss(animated: true, completion: nil) // foto seçildikten sonra galeriyi kapat
+    }
+    
+    func galleryController(_ controller: Gallery.GalleryController, didSelectVideo video: Gallery.Video) {
+        // kullanıcı video dizisi seçtiğinde bizi bilgilendirir
+    }
+    
+    func galleryController(_ controller: Gallery.GalleryController, requestLightbox images: [Gallery.Image]) {
+        // requestLightbox görüntüleri bir galeride gruplandırır ve bunları satır içinde kaydırma olarak modal bir açılır pencerede görüntüler. Kullanıcı görüntüleri tek tek açmadan fotoğraflar arasında gezinebilir.
+    }
+    
+    func galleryControllerDidCancel(_ controller: Gallery.GalleryController) {
+        // kullanıcı cancel a tıkladığında ne yapmak istiyorsak buraya örn galeriyi kapatma
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
