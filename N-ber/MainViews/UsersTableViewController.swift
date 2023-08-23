ab2f566c7bc01a1689142a2d85f0fabd3f4e74ff
SeymaArslan <seymarslannn@gmail.com>
2023-08-23 13:37:45 +0300

    Create refreshController and Profile View UI

--- N-ber/MainViews/UsersTableViewController.swift
+++ N-ber/MainViews/UsersTableViewController.swift
@@ -18,13 +18,23 @@ class UsersTableViewController: UITableViewController {
     //MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.refreshControl = UIRefreshControl()
+        self.tableView.refreshControl = self.refreshControl
         
         tableView.tableFooterView = UIView()
         
 //        createDummyUsers()
         setupSearchController()
         downloadUsers()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationItem.largeTitleDisplayMode = .always
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
     }
 
     // MARK: - Table view data source
@@ -40,13 +50,22 @@ class UsersTableViewController: UITableViewController {
         cell.configure(user: user)
         return cell
     }
+    
     //MARK: - Table view delegates
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let headerView = UIView()
         headerView.backgroundColor = UIColor(named: "tableviewBackgroundColor")
         return headerView
     }
-
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        //TODO: show profile view
+    }
     
     //MARK: - Download Users
     private func downloadUsers(){
@@ -74,6 +93,14 @@ class UsersTableViewController: UITableViewController {
             return user.username.lowercased().contains(searchText.lowercased())
         })
         tableView.reloadData()
+    }
+    
+    //MARK: - UIScrollViewDelegate
+    override func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) { // yukarıdan aşağı çekme işlemini kontrol ediyor
+        if self.refreshControl!.isRefreshing {
+            self.downloadUsers()
+            self.refreshControl!.endRefreshing()
+        }
     }
     
 }
---
+++ N-ber/SecondaryViews/ProfileTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileTableViewController.swift
+//  N-ber
+//
+//  Created by Seyma on 23.08.2023.
+//
+
+import UIKit
+
+class ProfileTableViewController: UITableViewController {
+
+    //MARK: - IBOutlets
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    
+    //MARK: - ViewLifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
