e236d6dede36205902c3b6e4b0358fb62e7cd148
SeymaArslan <seymarslannn@gmail.com>
2023-08-30 13:08:45 +0300

    Configuring messageView

--- N-ber/SecondaryViews/ChatViewController.swift
+++ N-ber/SecondaryViews/ChatViewController.swift
@@ -18,6 +18,10 @@ class ChatViewController: MessagesViewController {
     private var recipientId = ""
     private var recipientName = ""
     
+    let refreshController = UIRefreshControl()
+    
+    let micButton = InputBarButtonItem()
+    
     //MARK: - Inits
     init(chatId: String, recipientId: String, recipientName: String) {
         super.init(nibName: nil, bundle: nil) // so initialize our super view of our messenger messages, your controller as well
@@ -35,6 +39,45 @@ class ChatViewController: MessagesViewController {
         super.viewDidLoad()
        
     }
+    
+    //MARK: - Configurations
+    private func configureMessageCollectionView() { // set all delegates
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messageCellDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        
+        scrollsToBottomOnKeyboardBeginsEditing = true // the reason we want automatically scrolls to the last message
+        maintainPositionOnKeyboardFrameChanged = true
+        
+        messagesCollectionView.refreshControl = refreshController  // this is the display of the spins after the user pulls down the chatView
+        
+    }
 
-
+    private func configureMessageInputBar() { // message input power we get from our message
+        messageInputBar.delegate = self
+        let attachButton = InputBarButtonItem()
+        attachButton.image = UIImage(systemName: "plus.viewfinder")
+        attachButton.setSize(CGSize(width : 30, height: 30), animated: false)
+        attachButton.tintColor = .systemOrange
+        attachButton.onTouchUpInside { item in
+            print("ekle butonuna basıldı")
+        }
+        
+        // microphone
+        micButton.image = UIImage(systemName: "mic.fill")
+        micButton.setSize(CGSize(width: 30, height: 30), animated: false)
+        micButton.tintColor = .systemOrange
+        
+        // add gesture recognizer
+        
+        messageInputBar.setStackViewItems([attachButton], forStack: .left, animated: false)
+        
+        messageInputBar.setLeftStackViewWidthConstant(to: 36, animated: false) // control to constraints
+        
+        messageInputBar.inputTextView.isImagePasteEnabled = false // don't want to for the user to be able to paste the image inside our cell here, copy text but can't copy image
+        messageInputBar.backgroundView.backgroundColor = .systemBackground
+        messageInputBar.inputTextView.backgroundColor = .systemBackground
+        
+    }
 }
